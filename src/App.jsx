e8dee23f6a32a445be143121ac78c3b519633221
HensalDeon <hensaldeon777@gmail.com>
2025-08-07 20:58:37 +0530

    setuped utility styles

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,6 @@ import { Outlet } from "react-router-dom";
 function App() {
     return (
         <>
-            <nav></nav>
             <Outlet />
             <footer></footer>
         </>
--- src/components/NotFound.jsx
+++ src/components/NotFound.jsx
@@ -1,11 +1,12 @@
-import React from 'react'
+import React from "react";
 
 const NotFound = () => {
-  return (
-    <div>
-      NotFound
-    </div>
-  )
-}
+    return (
+        <section className={`bg-black h-screen w-screen flex flex-col justify-center items-center`}>
+            <h1 className={`neonText mx-auto text-center`}>404</h1>
+            <h2 className={`sub-head-text text-primary mx-auto text-center`}>Page Not Found</h2>
+        </section>
+    );
+};
 
-export default NotFound
+export default NotFound;
